9a1d7ff81a73f13eca6e5fe65c6f2c5fa9d0987d
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-09-14 15:33:51 +0200

    Init tailwind istall

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',  // Look for classes in all HTML and JS files in the src folder
+    './index.html',          // Include your main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
